ea3b6b27668d33c65d0b9df6dcdc1f5e3a16ad76
isaiatogia <m266390@usna.edu>
2023-10-25 11:45:58 -0400

    K_UP and K_DOWN

--- game4/game4.py
+++ game4/game4.py
@@ -20,9 +20,9 @@ pygame.display.set_caption("Using blit to draw tiles")
 
 def draw_background(screen):
     # Load our tiles from the assets folder
-    water = pygame.image.load("assets/sprites/water.png").convert()
-    sand = pygame.image.load("assets/sprites/sand_top.png").convert()
-    seagrass = pygame.image.load("assets/sprites/seagrass.png").convert()
+    water = pygame.image.load("../assets/sprites/water.png").convert()
+    sand = pygame.image.load("../assets/sprites/sand_top.png").convert()
+    seagrass = pygame.image.load("../assets/sprites/seagrass.png").convert()
 
     # make PNGs transparent
     sand.set_colorkey((0, 0, 0))
@@ -43,7 +43,7 @@ def draw_background(screen):
         screen.blit(seagrass, (x, screen_height - tile_size * 2 + 10))
 
     # draw the text #Load Game Font
-    custom_font = pygame.font.Font("assets/fonts/28 Days Later.ttf", 128)
+    custom_font = pygame.font.Font("../assets/fonts/28 Days Later.ttf", 128)
     text = custom_font.render("Chomp", True, (255, 29, 0))
     screen.blit(text, (screen_width / 2 - text.get_width() / 2, screen_height / 8 - text.get_height() / 2))
 
---
+++ game5/game_parameters.py
@@ -0,0 +1,8 @@
+#screen dimensions
+screen_width = 800
+screen_height = 600
+tile_size = 64
+
+MIN_SPEED = 0.5 #pixels per frame
+MAX_SPEED = 3
+PLAYER_SPEED = 1.5
