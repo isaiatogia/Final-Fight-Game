5ec8b0d9c0883979e6afbd55cd54a820626aac86
isaiatogia <m266390@usna.edu>
2023-11-15 22:29:16 -0500

    Put sprite on rectangles
    Fighter sprites have been scaled to corresponding rectangles

--- Street Kombat/main.py
+++ Street Kombat/main.py
@@ -21,9 +21,13 @@ WHITE = (255, 255, 255)
 
 #define fighter variables
 WARRIOR_SIZE = 162
-WARRIOR_DATA = [WARRIOR_SIZE]
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE,WARRIOR_SCALE, WARRIOR_OFFSET]
 WIZARD_SIZE =250
-WIZARD_DATA = [WIZARD_SIZE]
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
 
 #load background image
 bg_image = pygame.image.load("../Street Kombat/assets/images/background/background.jpg").convert_alpha()
