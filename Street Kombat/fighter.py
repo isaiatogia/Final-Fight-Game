26209424482f6fd598074809fa76ae34caaa66d9
isaiatogia <m266390@usna.edu>
2023-11-15 22:33:09 -0500

    Auto flip for fighter 1
    Fighter 1 always looks in the direction of fighter 2

--- Street Kombat/fighter.py
+++ Street Kombat/fighter.py
@@ -95,5 +95,6 @@ class Fighter():
 
 
     def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
-        surface.blit(self.image, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
